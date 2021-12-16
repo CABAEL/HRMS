11abc517e24606042605c1a1a5ed5ae81308fb30
CABAEL <vallesmark15@gmail.com>
2021-12-16 18:59:55 +0800

    admin seeder

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert
+        (
+            [
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
+            'role' => 'admin',
+            ],
+        );
+    }
+}
